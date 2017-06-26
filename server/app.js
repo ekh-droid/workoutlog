c2e1eae116a017c036a43eb2157e85215660eb35
Elena Hickey <subhickeyelena@gmail.com>
2017-06-26 11:02:52 -0400

    connected users to the database & various notes

--- server/app.js
+++ server/app.js
@@ -5,13 +5,71 @@ var app = express();
 //this creates an instance of express that can be used to call
 //various express functions that build a server
 
+var bodyParser = require('body-parser');
 
-app.use('/api/test', function(req,res){
+//header request for middleware
+app.use(require('./middleware/header'));
+
+app.use("/api/test", function(req, res) {
 	res.send("Hello World");
 });
+
 
 //this function starts the port open
 app.listen(3000,function(){
 		console.log("app is listening on 3000");
 });
 
+//setting up a connection
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('workoutlog', 'postgres', 'bandaid4', {
+	host: 'localhost',
+	dialect: 'postgres'
+
+});
+
+sequelize.authenticate().then(
+		function() {
+				console.log('connected to workoutlog postgres db');
+			},
+			function(err){
+				console.log(err);
+			}
+);
+//data model, username & pass, model.table and 'user' represents the table	
+
+//a sequelize method, for the above user object.
+var User = sequelize.define('user', {
+			username: Sequelize.STRING,
+			passwordhash: Sequelize.STRING
+
+	});
+
+User.sync();
+
+
+//**THIS ({force:true}) DROPS THE USERS FROM THE DATABASE!***
+//User.sync({force: true});
+app.use(bodyParser.json());
+
+app.post('/api/user', function(req, res){
+	var username = req.body.user.username;
+	var pass = req.body.user.password;
+	//need to create a user object and use sequelize to put that user itno
+
+User.create({
+		username: username,
+		passwordhash: pass
+}).then(
+
+			function createSuccess(user){
+				res.json({
+						user: user,
+						message:'u made an account pal'
+				});
+			},
+			function createError(err){
+					res.send(500, err.message);
+			}
+	);		
+});
---
+++ server/middleware/header.js
@@ -0,0 +1,8 @@
+
+//header created nodule file, a cross origin request connecting the server & client together
+// the header written this way allows access by all origins (*)
+module.exports = function(req, res, next){
+	//the "*" allows any other origin to acess the site, 'wildcard'
+	res.header("access-control-allow-origin","*");
+	next();
+};
